53b56cc4133a23905c090f93a4a7be0757325253
Alexandr <118203209+BandrD@users.noreply.github.com>
2024-05-13 22:12:25 +0500

    Update data_creation.py

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -2,15 +2,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
 
-# Загрузка данных (можно заменить на выкачивание из интернета)
+# Загрузка данных
 data = pd.read_csv("build_price.csv")
 
 # Создание папок "train" и "test", если они не существуют
 os.makedirs('train', exist_ok=True)
 os.makedirs('test', exist_ok=True)
 
+# Удаление столбцов 'timestamp' и 'sub_area'
+build_price = data.drop(columns=['timestamp', 'sub_area'])
+
 # Разделение данных на обучающий и тестовый наборы
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+train_data, test_data = train_test_split(build_price, test_size=0.2, random_state=42)
 
 # Сохранение обучающего и тестового наборов данных
 train_data.to_csv('train/build_price_train.csv', index=False)
